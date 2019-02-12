6410801b53326d86e53b17213ebb2032c247dfed
Chris3691 <42034762+Chris3691@users.noreply.github.com>
2019-02-12 18:59:25 +0000

    Update app.py

--- app.py
+++ app.py
@@ -10,8 +10,7 @@ from config import config
 app = dash.Dash(__name__)
 server = app.server
 app.config.suppress_callback_exceptions = True
-app.css.config.serve_locally = True
-app.scripts.config.serve_locally = True
+
 
 
 # config
